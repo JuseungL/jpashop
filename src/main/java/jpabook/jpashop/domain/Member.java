14424a2eacf03381c4f068fb58a257403010ed3a
JuseungL <juseung0619@gmail.com>
2023-08-02 23:44:37 +0900

    Entity Class

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter @Getter
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    //연관관계 주인이 아님을 알려줌. Order테이블의 member필드에 의해 매핑 된거야. @JoinColumn(name = "member_id")와 짝
+    @OneToMany(mappedBy = "member")
+    @JoinColumn(name = "member_id")
+    private List<Order> orders = new ArrayList<>();
+
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; //주문시간
+
+    private OrderStatus  status; //주문상태 ORDER, CANCEL
+
+}
